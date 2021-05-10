2b4e1afb7cd2012a5c1c9f47473db8fb57f6ca02
Sergii Sadovyi <1175636+sergsadovyi@users.noreply.github.com>
2021-05-10 08:05:36 +0300

    fix(cloudrun): cache label value

--- src/deploy/cloudrun.ts
+++ src/deploy/cloudrun.ts
@@ -39,7 +39,7 @@ export const cloudrun = async (deployment: Deployment, destination: CloudRunDest
     if (!serviceKnativeSpec.metadata?.labels) {
         serviceKnativeSpec.metadata.labels = {};
     }
-    serviceKnativeSpec.metadata.labels['build-date'] = (new Date()).toISOString();
+    serviceKnativeSpec.metadata.labels['build-date'] = (new Date()).getTime();
 
     const auth = new google.auth.GoogleAuth({
         credentials: {
